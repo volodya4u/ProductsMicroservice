06b69d8238a59bc584c248aef8107f4fc0ef0fef
volodymyrhrabovenskyi <volodya4u@gmail.com>
2025-06-29 22:29:03 +0300

    Send kafka event synchronously

--- src/main/java/com/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
@@ -1,13 +1,51 @@
 package com.appsdeveloperblog.ws.products.service;
 
+import com.appsdeveloperblog.ws.products.ProductCreatedEvent;
 import com.appsdeveloperblog.ws.products.rest.CreateProductRestModel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+
+    public ProductServiceImpl(KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Override
     public String createProduct(CreateProductRestModel productRestModel) {
-        return "";
+
+        String productId = UUID.randomUUID().toString();
+
+        // TODO: Persist Product Details info database table before publishing an Event
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(productId,
+                productRestModel.getTitle(),
+                productRestModel.getPrice(),
+                productRestModel.getQuantity());
+
+        CompletableFuture<SendResult<String, ProductCreatedEvent>> future =
+                kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent);
+
+        future.whenComplete((result, exception) -> {
+
+            if (exception != null) {
+                log.error("Error while sending product created event: {}", exception.getMessage());
+            } else {
+                log.info("Successfully send product created event: {}", result.getRecordMetadata());
+            }
+        });
+
+        future.join();
+
+        return productId;
     }
 }
