14b6df062751d14ee48745797d43708fe1d15ba0
volodymyrhrabovenskyi <volodya4u@gmail.com>
2025-06-29 20:56:38 +0300

    Add model and ProductController

---
+++ src/main/java/com/appsdeveloperblog/ws/products/rest/CreateProductRestModel.java
@@ -0,0 +1,17 @@
+package com.appsdeveloperblog.ws.products.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CreateProductRestModel {
+
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+}
---
+++ src/main/java/com/appsdeveloperblog/ws/products/rest/ProductController.java
@@ -0,0 +1,21 @@
+package com.appsdeveloperblog.ws.products.rest;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @PostMapping
+    public ResponseEntity<String> createProduct(@RequestBody CreateProductRestModel product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body("");
+    }
+
+
+}
